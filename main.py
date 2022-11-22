2d9050ecf47a4854bdbaa4cb41a9ebcc3ac3a651
Hank <71367215+mkrxp123@users.noreply.github.com>
2022-11-22 18:02:01 +0000

    adjust indent and ajax data

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.models import TextSendMessage, ImageSendMessage, MessageEvent, TextMessage
 from linebot.exceptions import LineBotApiError, InvalidSignatureError
 from flask import Flask, request, abort, render_template, redirect, url_for, jsonify
-from utility import getKey, timetable
+from utility import getKey, ajaxResponse, timetable
 from rich_menu import rich_menu
 import re
 
 config = getKey()
-reminder_db = timetable()
+# uncomment after you finish database connection
+# reminder_db = timetable()
 
 line_bot_api = LineBotApi(config["Channel access token"])
 handler = WebhookHandler(config["Channel secret"])
@@ -17,62 +18,60 @@ app = Flask(__name__)
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
 def callback():
-  signature = request.headers['X-Line-Signature']
-
-  body = request.get_data(as_text=True)
-  app.logger.info("Request body: " + body)
-  try:
-    handler.handle(body, signature)
-  except InvalidSignatureError:
-    abort(400)
-
-  return 'OK'
+    signature = request.headers['X-Line-Signature']
+    
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    
+    return 'OK'
 
 
 @app.route("/")
 def home():
-  '''
-  redirect user to nav.html
-  '''
-  return redirect(url_for('nav'))
+    '''
+    redirect user to nav.html
+    '''
+    return redirect(url_for('nav'))
 
 
 @app.route("/nav")
 def nav():
-  liff_id = config["Liff ID"]
-  return render_template("nav.html", liff_id=liff_id)
+    liff_id = config["Liff ID"]
+    return render_template("nav.html", liff_id=liff_id)
 
 
 @app.route("/fill-form", methods=["POST"])
 def fill_form():
-  form = request.json
-  print(form)
-  '''
-	implement insert time table sql here
-	form structure:
-	{
-		"begindate"		: "<string>",
-		"enddate"		: "<string>",
-		"timepicker0"	: "<string>",
-		"timepicker1"	: "<string>",
-		...
-	}
- 	'''
-
-  response = jsonify({'msg': 'fill form successfully'})
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.status_code = 200
-  return response
+    form = request.json
+    print(form)
+    '''
+    implement insert time table sql here
+    form structure:
+    {
+        "user_id"    : "<string>"
+        "begindate"  : "<string>",
+        "enddate"    : "<string>",
+        "timepicker0": "<string>",
+        "timepicker1": "<string>",
+        ...
+    }
+    '''
+    
+    return ajaxResponse({'msg': 'fill form successfully'})
 
 
 # see https://xiaosean.github.io/chatbot/2018-04-19-LineChatbot_usage/
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-  # get user id when reply
-  user_id = event.source.user_id
-  print("user_id =", user_id)
-  msg = TextSendMessage(text=f'https://liff.line.me/{config["Liff ID"]}')
-  line_bot_api.reply_message(event.reply_token, msg)
+    # get user id when reply
+    user_id = event.source.user_id
+    print("user_id =", user_id)
+    msg = TextSendMessage(text=f'https://liff.line.me/{config["Liff ID"]}')
+    line_bot_api.reply_message(event.reply_token, msg)
 
 
 if __name__ == '__main__':
--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
 import json, sqlite3
+from flask import jsonify
 sqlite3.enable_callback_tracebacks(True)
 
 def getKey():
@@ -6,11 +7,17 @@ def getKey():
         config = json.load(f)
     return config
 
+def ajaxResponse(dict):
+    response = jsonify(dict)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.status_code = 200
+    return response
+
 class timetable:
     def __init__(self):
         self.db = sqlite3.connect("./database/timetable.db")
         self.db.row_factory = sqlite3.Row
-        with open(f'./database/schema.sql', 'r') as f:
+        with open('./database/schema.sql', 'r') as f:
             self.db.cursor().executescript(f.read())
         self.db.cursor().execute("PRAGMA foreign_keys=ON")
         self.db.commit()
