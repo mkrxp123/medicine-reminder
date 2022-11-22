13bc716a7c5a91bc61ef6857a91f9207e35f3a3f
JimpLin <84771981+JimpLin@users.noreply.github.com>
2022-11-22 19:17:57 +0800

    update schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,24 @@
-CREATE TABLE
-    if NOT EXISTS Users(
-        UID INTEGER PRIMARY KEY NOT NULL
+CREATE TABLE 
+    if NOT EXISTS public."Users"(
+        "LineID" character(33) COLLATE pg_catalog."default" NOT NULL,
+        "UserName" character varying(50) COLLATE pg_catalog."default" NOT NULL,
+        CONSTRAINT "Users_pkey" PRIMARY KEY ("LineID")
+    );
 
+CREATE TABLE 
+    if NOT EXISTS public."Reminders"(
+        "Title" character varying(50) COLLATE pg_catalog."default" NOT NULL,
+        "ReminderID" character(4) COLLATE pg_catalog."default" NOT NULL,
+        "UserName" character(50) COLLATE pg_catalog."default" NOT NULL,
+        "Prescription" bytea,
+        "Picture" bytea,
+        "TotalTimes" integer NOT NULL,
+        CONSTRAINT "Reminders_pkey" PRIMARY KEY ("ReminderID")
+    );
+
+CREATE TABLE 
+    if NOT EXISTS public."RemindTimes"(
+        "ReminderID" character(4) COLLATE pg_catalog."default" NOT NULL,
+        "RemindTime" time without time zone NOT NULL,
+        CONSTRAINT "RemindTimes_pkey" PRIMARY KEY ("ReminderID", "RemindTime")
     );
