4b6af689efee5d07d7ebce1307fc2536af36618b
JimpLin <84771981+JimpLin@users.noreply.github.com>
2022-11-26 22:00:58 +0800

    + GetMedicine/Hospital/RemindDate - Prescription

--- database/schema.sql
+++ database/schema.sql
@@ -6,22 +6,24 @@ CREATE TABLE
     );
 
 CREATE TABLE 
-    if NOT EXISTS public."Reminders"(
-        "Title" character varying(50) COLLATE pg_catalog."default" NOT NULL,
-        "ReminderID" character(4) COLLATE pg_catalog."default" NOT NULL,
-        "UserName" character varying(50) COLLATE pg_catalog."default" NOT NULL,
-        "Prescription" bytea,
+	if NOT EXISTS public."Reminders"(
+    	"Title" character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    	"ReminderID" integer NOT NULL,
+    	"UserName" character varying(50) COLLATE pg_catalog."default" NOT NULL,
         "Picture" bytea,
         "TotalTimes" integer NOT NULL,
+        "Hospital" character varying(50) COLLATE pg_catalog."default",
         "GroupID" character(33) COLLATE pg_catalog."default" NOT NULL,
-        CONSTRAINT "Reminders_pkey" PRIMARY KEY ("ReminderID")
-    );
+        "GetMedicine" boolean NOT NULL,
+        CONSTRAINT "Reminders_pkey1" PRIMARY KEY ("ReminderID")
+	);
 
 CREATE TABLE 
     if NOT EXISTS public."RemindTimes"(
-        "ReminderID" character(4) COLLATE pg_catalog."default" NOT NULL,
+        "ReminderID" integer NOT NULL,
         "RemindTime" time without time zone NOT NULL,
-        CONSTRAINT "RemindTimes_pkey" PRIMARY KEY ("ReminderID", "RemindTime")
+        "RemindDate" date NOT NULL,
+        CONSTRAINT "RemindTimes_pkey1" PRIMARY KEY ("ReminderID", "RemindTime", "RemindDate")
     );
 
 CREATE TABLE 
